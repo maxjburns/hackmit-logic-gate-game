af299b76f417fc784601a6f636134554f43b1f8d
michelle-zong <71539265+michelle-zong@users.noreply.github.com>
2020-09-19 15:43:54 -0400

    Add files via upload

---
+++ Sprite.java
@@ -0,0 +1,19 @@
+public class Sprite
+{
+  private int x;
+  private int y;
+  private String image;
+  
+  public Sprite(int xCoor, int yCoor, String s)
+  {
+    x = xCoor;
+    y = yCoor;
+    image = s;
+  }  
+  
+  public void setLocation(int xNew, int yNew)
+  {
+    x = xNew;
+    y = yNew;
+  }  
+}  
