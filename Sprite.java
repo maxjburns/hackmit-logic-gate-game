b7cf4602e2dde7a9dcabde23eb941f746774c368
CrystalEye42 <49692820+CrystalEye42@users.noreply.github.com>
2020-09-19 16:46:03 -0400

    Add files via upload
    Modifications to Sprite.java

--- Sprite.java
+++ Sprite.java
@@ -1,5 +1,42 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.awt.*;
+
 public class Sprite
 {
+<<<<<<< HEAD
+    private int x;
+    private int y;
+    private BufferedImage image;
+
+    public Sprite(int xCoor, int yCoor, String s)
+    {
+        x = xCoor;
+        y = yCoor;
+        image = loadSprite(s);
+    }  
+
+    public BufferedImage loadSprite(String file) {
+        
+        BufferedImage sprite = null;
+
+        try {
+            sprite = ImageIO.read(getClass().getResource(file+".png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return sprite;
+    }
+
+    public void setLocation(int xNew, int yNew)
+    {
+        x = xNew;
+        y = yNew;
+    }  
+=======
   private int x;
   private int y;
   private String image;
@@ -17,4 +54,5 @@ public class Sprite
     x = xNew;
     y = yNew;
   }  
+>>>>>>> 863232a16a613158a043fa05d022289505aac586
 }  
