5dd083b62f5a173e33c2cfaca1863219cabe0601
CrystalEye42 <49692820+CrystalEye42@users.noreply.github.com>
2020-09-19 19:16:45 -0400

    Add files via upload

--- Display.java
+++ Display.java
@@ -13,22 +13,72 @@ import javax.swing.Timer;
 import java.util.*;
 import java.io.*;
 
-public class Display
+public class Display extends JPanel implements ActionListener, KeyListener
 {
-    private Image img;
-    private static int WIDTH, HEIGHT;
-    
-    public static void main(String[] args) throws IOException
-    {
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        WIDTH = (int) (screenSize.getWidth()*3/4);
-        HEIGHT = (int) (screenSize.getHeight()*3/4);
+    private Image background;
+
+    public Display(int WIDTH, int HEIGHT) {
+        JFrame frame = new JFrame();
+
+        frame.setPreferredSize(new Dimension(WIDTH,HEIGHT));
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.getContentPane().add();
+        frame.add(this);
+        frame.setVisible(true);
+        System.out.println("here2");
+
+        BufferedImage rawBackground = null;
+        try {
+            rawBackground = ImageIO.read(getClass().getResource("BackgroundHack.png"));
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException("probably file not found, if not try getting latest one from github");
+        }
         
+        background = rawBackground.getScaledInstance(WIDTH, HEIGHT, Image.SCALE_DEFAULT);;
+ 
         
+        addKeyListener(this);
+
+        setFocusable(true);
+
+        setFocusTraversalKeysEnabled(false);
+
+        repaint();
     }
 
-    public Display() throws IOException {
-        img = ImageIO.read(getClass().getResource("Background.png"));
-        
+    public void paintComponent(Graphics g)
+    {
+        Graphics2D g2d = (Graphics2D) g;
+
+        super.paintComponent(g);
+        //g.setColor(new Color(0,0,0));
+        //g.drawLine(0,0,100,100);
+        //System.out.println("here");
+        g.drawImage(background, 0, 0, this); //paints background
+       // System.out.println("here");
+    }
+
+    public void actionPerformed(ActionEvent actionEvent)
+    {
+        //not implemented
+    }
+
+    public void keyTyped(KeyEvent keyEvent)
+    {
+
+    }
+
+    public void keyPressed(KeyEvent e)
+    {
+
+    }
+
+    public void keyReleased(KeyEvent keyEvent)
+    {
+
     }
 }
--- Game.java
+++ Game.java
@@ -1,11 +1,36 @@
+import javax.imageio.ImageIO;
+import javax.sound.midi.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.awt.Font;
+import java.io.IOException;
+import javax.swing.Timer;
+import java.util.*;
+import java.io.*;
+
 public class Game
 {
-  private Monster[] monsters;
-  private Monster output;
-  
-  public Game(Monster[] ms, Monster m)
-  {
-    monsters = ms;
-    output = m;
-  }  
+    private Monster[] monsters;
+    private Monster output;
+    private static int WIDTH, HEIGHT;
+    
+    public static void main(String[] args) throws IOException
+    {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        WIDTH = (int) (screenSize.getWidth()*3/4);
+        HEIGHT = (int) (screenSize.getHeight()*3/4); //scales to size of screen
+
+        Display d = new Display(WIDTH, HEIGHT);
+    }
+
+    public Game(Monster[] ms, Monster m)
+    {
+        monsters = ms;
+        output = m;
+    }  
 }  
